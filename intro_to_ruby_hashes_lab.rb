19074c0216d38e15210638ba72f93eace9ff6ea2
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-28 18:13:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ def actor
   actor = {name: "Dwayne The Rock Johnson"}
 end
 
-# Add empty key-value pair to the monopoly hash
+# Add empty hash to the monopoly hash
 def monopoly
 	monopoly = {railroads: {}}
 end
